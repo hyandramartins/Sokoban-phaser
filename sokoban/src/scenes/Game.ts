94c281a7d45b566ccb3e31ae43f356b0f8b37f64
hyandramartins <hyandramartins111@gmail.com>
2026-03-23 11:59:25 -0300

    Adicionado logica para caixas e alvos de cores correspondentes

--- sokoban/src/scenes/Game.ts
+++ sokoban/src/scenes/Game.ts
@@ -1,10 +1,15 @@
 import Phaser from 'phaser'
+import * as Colors from '../boxesAndTargets/Colors'
+import { boxColorToTargetColor } from '../utils/ColorTarget'
 
 export default class Game extends Phaser.Scene {
 
     private player?: Phaser.GameObjects.Sprite
-    private boxes?: Phaser.GameObjects.Sprite[] = []
+    //private blueBoxes?: Phaser.GameObjects.Sprite[] = []
     private layer?: Phaser.Tilemaps.TilemapLayer
+
+    private targetsConveredByColor: { [key: number]: number } = {} //caixa e qtde de alvos daquela cor que estão preenchidos
+    private boxesByColor: { [key: number]: Phaser.GameObjects.Sprite[] } = {} //caixas e suas cores
 
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
 
@@ -26,12 +31,44 @@ export default class Game extends Phaser.Scene {
         if (this.input.keyboard) {
             this.cursors = this.input.keyboard.createCursorKeys()
         }
+        /*
+        const floorLevel = [
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90],
+            [90, 90, 90, 90, 90, 90, 90, 90]
+        ];
 
+        //Criar o mapa de dados do chão
+        const floorMap = this.make.tilemap({
+            data: floorLevel,
+            tileWidth: 64,
+            tileHeight: 64
+        });
+        const floorTileset = floorMap.addTilesetImage('tiles')
+
+        //Criar a camada (ela nasce no Depth 0 por padrão)
+
+        if (!floorTileset) {
+            throw new Error("Tileset não encontrado")
+        }
+        const floorLayer = floorMap.createLayer(0, floorTileset, 0, 0);
+
+        //Mudar a profundidade para -1 (atrás de tudo)
+        if (floorLayer) {
+            floorLayer.setDepth(-1);
+        }
+            */
         const level = [
             [100, 100, 100, 100, 100, 100, 100, 100],
+            [100, 0, 0, 0, 0, 0, 52, 100],
+            [100, 6, 7, 8, 9, 10, 0, 100],
+            [100, 25, 38, 51, 64, 77, 0, 100],
             [100, 0, 0, 0, 0, 0, 0, 100],
-            [100, 0, 0, 0, 0, 0, 0, 100],
-            [100, 0, 0, 51, 8, 0, 52, 100],
             [100, 0, 0, 0, 0, 0, 0, 100],
             [100, 0, 0, 0, 0, 0, 0, 100],
             [100, 100, 100, 100, 100, 100, 100, 100],
@@ -48,7 +85,7 @@ export default class Game extends Phaser.Scene {
         if (!tileset) {
             throw new Error("Tileset não encontrado")
         }
-        
+
         const layer = map.createLayer(0, tileset, 0, 0); //não ceita null
         if (!layer) throw new Error("Não foi possível criar a layer");
         this.layer = layer;
@@ -59,8 +96,8 @@ export default class Game extends Phaser.Scene {
 
         this.createPlayerAnims()
 
-        this.boxes = this.layer?.createFromTiles(8, 0, { key: 'tiles', frame: 8 })
-
+        //this.blueBoxes = this.layer?.createFromTiles(8, 0, { key: 'tiles', frame: 8 })
+        this.extractBoxes(this.layer)
 
     }
 
@@ -79,7 +116,7 @@ export default class Game extends Phaser.Scene {
             const ny = this.player.y
 
             //const box = this.getBoxAt(this.player.x - 64, this.player.y) //ver o que tem na posição para onde o player está indo por isso -64
-        
+
             //console.dir(box)
 
             const baseTween = {
@@ -106,7 +143,7 @@ export default class Game extends Phaser.Scene {
             })
         }
         else if (justUp) {
-            const nx = this.player.x 
+            const nx = this.player.x
             const ny = this.player.y - 64
 
             const baseTween = {
@@ -119,7 +156,7 @@ export default class Game extends Phaser.Scene {
             })
         }
         else if (justDown) {
-            const nx = this.player.x 
+            const nx = this.player.x
             const ny = this.player.y + 64
 
             const baseTween = {
@@ -139,30 +176,64 @@ export default class Game extends Phaser.Scene {
         }*/
     }
 
+    private extractBoxes(layer: Phaser.Tilemaps.TilemapLayer) {
+        const boxColors = [
+            Colors.BoxOrange,
+            Colors.BoxRed,
+            Colors.BoxBlue,
+            Colors.BoxGreen,
+            Colors.BoxGrey
+        ]
+
+        boxColors.forEach(color => {
+            this.boxesByColor[color] = this.layer?.createFromTiles(color, 0, { key: 'tiles', frame: color }) as Phaser.GameObjects.Sprite[]
+        })
+        console.dir(this.boxesByColor)
+    }
+
     private hasWallAt(x: number, y: number) {
-        if(!this.layer){
+        if (!this.layer) {
             return undefined
         }
         const tile = this.layer.getTileAtWorldXY(x, y) //pega o tile que está na posição (x, y) no mundo.
-        if(!tile){ // se não tiver tile nessa posição retorna false
+        if (!tile) { // se não tiver tile nessa posição retorna false
             return false
         }
         return tile.index === 100 // tile 100 é uma parede, retorna true se for uma parede ou false caso contrário
     }
 
     private tweenMove(x: number, y: number, baseTween: any, onStart: () => void) {
-        if(this.tweens.isTweening(this.player!)){ //se o player já estiver se movendo, não faz nada
+        if (this.tweens.isTweening(this.player!)) { //se o player já estiver se movendo, não faz nada
             return
         }
         const hasWall = this.hasWallAt(x, y)
 
-        if(hasWall){
-                return
+        if (hasWall) {
+            return
         }
 
-        const box = this.getBoxAt(x, y)
-        if (box) {
-            this.tweens.add(Object.assign({}, baseTween, { targets: box })) 
+        const boxData = this.getBoxDataAt(x, y)
+        if (boxData) {
+            const box = boxData.box
+            const boxColor = boxData.color
+            const targetColor = boxColorToTargetColor(boxColor)
+
+            const coveredTarget = this.hasTargetAt(box.x, box.y, targetColor) //verifica se a caixa que vai ser empurrada está em cima de um alvo, se sim, diminui a contagem de alvos preenchidos, porque a caixa vai sair de cima do alvo
+            if (coveredTarget) {
+                this.changeCountForColor(targetColor, -1)
+            }
+
+            this.tweens.add(Object.assign({}, baseTween, {
+                targets: box,
+                onComplete: () => {
+                    const coveredTarget = this.hasTargetAt(box.x, box.y, targetColor)
+                    //console.log(coveredTarget)
+                    if (coveredTarget) {
+                        this.changeCountForColor(targetColor, 1)
+                    }
+                    console.dir(this.targetsConveredByColor)
+                }
+            }))
         }
 
         this.tweens.add(Object.assign({}, baseTween, {
@@ -175,6 +246,25 @@ export default class Game extends Phaser.Scene {
         }))
     }
 
+    private hasTargetAt(x: number, y: number, tileIndex: number) {
+        if (!this.layer) {
+            return false
+        }
+
+        const tile = this.layer.getTileAtWorldXY(x, y)
+        if (!tile) {
+            return false
+        }
+        return tile.index === tileIndex
+    }
+
+    private changeCountForColor(color: number, change: number) {
+        if (!(color in this.targetsConveredByColor)) { //se ainda não tem o id da caixa para cor, inicializa com 0
+            this.targetsConveredByColor[color] = 0
+        }
+        this.targetsConveredByColor[color] += change
+    }
+
     private stopPlayerAnimation() {
         if (!this.player) {
             return
@@ -185,11 +275,26 @@ export default class Game extends Phaser.Scene {
         }
     }
 
-    private getBoxAt(x: number, y: number) {//existe uma caixa nessa posição? retorna a caixa
-        return this.boxes?.find(box => {   // retorna a caixa que tem o retangulo que contém as coordenadas x e y
-            const rect = box.getBounds()   //pega o retangulo da caixa
-            return rect.contains(x, y)     // retorna true ou false --> o retangulo contém as coordenadas x e y?
-        })
+    private getBoxDataAt(x: number, y: number) {//existe uma caixa nessa posição? retorna a caixa
+        const keys = Object.keys(this.boxesByColor) //pega as cores das caixas
+        for (let i = 0; i < keys.length; i++) {
+            const color = Number(keys[i])
+            const box = this.boxesByColor[color].find(box => {
+                const rect = box.getBounds()   //pega o retangulo da caixa
+                return rect.contains(x, y)     // retorna true ou false --> o retangulo contém as coordenadas x e y?
+            })
+
+            if (!box) {
+                continue
+            }
+
+            return {
+                box,
+                color: Number(color)
+            }
+        }
+
+        return undefined
     }
 
     private createPlayerAnims() {
