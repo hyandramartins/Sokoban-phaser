25ee5acc79bd3a68bd08145753a010b900e14063
Catharina Napoleão <99421377+ca-th@users.noreply.github.com>
2026-05-11 19:37:30 -0300

    Update

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -9,15 +9,13 @@ export default class BootScene extends Phaser.Scene {
         const width = this.scale.width;
         const height = this.scale.height;
 
-        // Texto centralizado na tela
         this.add.text(width / 2, height / 2, 'Clique em qualquer lugar para Iniciar', {
             fontSize: '24px',
             color: '#ffffff'
         }).setOrigin(0.5);
 
-        // O evento de clique (pointerdown) satisfaz a regra do navegador
         this.input.once('pointerdown', () => {
-            this.scene.start('IntroScene'); // Chama a cena do trailer logo após o clique
+            this.scene.start('IntroScene');
         });
     }
 }
